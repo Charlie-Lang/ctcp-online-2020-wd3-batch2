8a4070f8cfc3b3bb422722a8967e99eb6fb72e9f
Charlie-Lang <68047904+Charlie-Lang@users.noreply.github.com>
2020-12-17 12:27:31 +0800

    Add files via upload

---
+++ sqlDiscuss/day8.sql
@@ -0,0 +1,20 @@
+-- http://127.0.0.1/phpmyadmin/
+CREATE TABLE tbl_sample1 (fld_name VARCHAR(20));
+
+CREATE TABLE tbl_sample2 (
+	fld_product VARCHAR(75) NOT NULL
+	, fld_category VARCHAR(30) NOT NULL
+	, fld_price FLOAT(20,4) NOT NULL
+	, fld_quantity INT NOT NULL 
+) ENGINE = InnoDB;
+
+--INSERT = INPUT DATA TO 
+
+CREATE TABLE tbl_sample3 (
+	fld_id INT NOT NULL AUTO_INCREMENT 
+	, fld_product VARCHAR(75) NOT NULL 
+	, fld_category VARCHAR(30) NOT NULL 
+	, fld_price FLOAT(20,4) NOT NULL 
+	, fld_quantity INT NOT NULL 
+	, PRIMARY KEY (fld_id)
+) ENGINE = InnoDB;
